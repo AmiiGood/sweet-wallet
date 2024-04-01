6a01c4cbab84428c1578dd688f9e5255d1aba2b6
Alexis <alexisalvarez1234563@outlook.com>
2024-04-01 13:01:49 -0600

    miCartera iniciado

--- scripts/index.js
+++ scripts/index.js
@@ -1,19 +1,36 @@
 const loginForm = document.getElementById('loginForm');
 
-loginForm.addEventListener('submit', function (event) {
+loginForm.addEventListener('submit', async function (event) {
     event.preventDefault();
 
-    let user = document.getElementById('user').value;
+    let email = document.getElementById('email').value;
     let password = document.getElementById('password').value;
 
-    if (user === 'admin' && password === 'root') {
-        window.location.href = './menuPrincipal/menuPrincipal.html';
-        window.history.replaceState(null, null, window.location.href);
-    } else {
-        alert('Nombre de usuario o contrasena incorrectos');
-    }
+    try {
+        const response = await fetch('http://localhost:8080/login', {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify({ email, password })
+        });
 
+        const data = await response.json();
+
+        if (response.ok) {
+            const token = data.token;
+            localStorage.setItem('token', token);
+            window.location.href = './menuPrincipal/menuPrincipal.html';
+            window.history.replaceState(null, null, window.location.href);
+        } else {
+            alert(data.msg || 'Error en la autenticación');
+        }
+    } catch (error) {
+        console.error('Error:', error);
+        alert('Hubo un error en la solicitud');
+    }
 });
+
 
 window.addEventListener('load', function () {
     window.history.pushState(null, null, window.location.href);
@@ -23,7 +40,7 @@ window.addEventListener('load', function () {
     };
 });
 
-window.onload = function(){
-    let user = document.getElementById('user');
-    user.focus();
+window.onload = function () {
+    let correo = document.getElementById('email');
+    correo.focus();
 }
